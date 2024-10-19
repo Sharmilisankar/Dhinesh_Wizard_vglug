e1fa28ff334b3f48e761c2e5f273eb0f865bdadc
Sharmilisankar <165658117+Sharmilisankar@users.noreply.github.com>
2024-10-19 20:07:35 +0530

    Add files via upload

---
+++ dict1.py
@@ -0,0 +1,13 @@
+# Initialize an empty dictionary
+user_info = {}
+
+# Get three inputs from the user
+user_info['name'] = input("Enter your name: ")
+user_info['age'] = input("Enter your age: ")
+user_info['city'] = input("Enter your city: ")
+
+# Display the resulting dictionary
+print("\nUser Information:")
+print(user_info)# Initialize an empty dictionary
+user_info = {}
+
